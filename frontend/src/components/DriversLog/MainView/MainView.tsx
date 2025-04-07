d185fb6953153c2346713b7d80c186c9de0602c1
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2025-04-08 00:31:55 +0200

    view for the selected content

--- frontend/src/components/DriversLog/MainView/MainView.tsx
+++ frontend/src/components/DriversLog/MainView/MainView.tsx
@@ -1,9 +1,16 @@
 import { Text } from "@mantine/core";
-import { useMainContentStore } from "../../../hooks/useMainContentStore";
+import { useMainContentStore } from "../../../stores/useMainContentStore";
+import { Routes } from "./Routes/Routes";
+import { NewRouteModal } from "./NewRouteModal/NewRouteModal";
 
-export function MainView(){
-    const content = useMainContentStore((state) => state.content); // Zustand-Hook zum Abrufen des JWT-Token
+export function MainView() {
+  const content = useMainContentStore((state) => state.content); // Zustand-Hook zum Abrufen des JWT-Token
 
-    return (<>{content === "Kalender" && (<Text>Kalendar</Text>)}
-    {content === "Strecken" && (<Text>Strecken</Text>)}</>);
+  return (
+    <>
+      {content === "Kalender" && <Text>Kalendar</Text>}
+      {content === "Strecken" && <Routes />}
+      <NewRouteModal />
+    </>
+  );
 }
