a704de9ae2195af050b3a3b80c124aa405b1fd5b
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2025-04-08 00:09:25 +0200

    -

--- backend/src/main/java/com/example/security/config/CorsConfig.java
+++ backend/src/main/java/com/example/security/config/CorsConfig.java
@@ -9,6 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                
                 .allowedOrigins("http://localhost:3000") // React-URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
