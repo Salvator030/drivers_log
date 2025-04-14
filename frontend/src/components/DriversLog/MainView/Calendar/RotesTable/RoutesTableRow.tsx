671b8eabf9cd1e92212f2bd75ba804167a59d6eb
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2025-04-14 03:13:51 +0200

    add HoverCard

--- frontend/src/components/DriversLog/MainView/Calendar/RotesTable/RoutesTableRow.tsx
+++ frontend/src/components/DriversLog/MainView/Calendar/RotesTable/RoutesTableRow.tsx
@@ -1,4 +1,4 @@
-import { Table } from "@mantine/core";
+import { Grid, Group, HoverCard, Table } from "@mantine/core";
 import { FullValueRoute } from "../../../../../types";
 import { useState } from "react";
 
@@ -14,15 +14,59 @@ export function RoutesTableRow(probs: TableRowProps) {
       style={selected ? { backgroundColor: "gray" } : {}}
       onClick={() => {
         setSelected(!selected);
-        console.log("id: ",probs.route.routeId)
+        console.log("id: ", probs.route.routeId);
         if (probs.route.routeId !== null) {
           probs.changeSelectedRoutes(probs.route.routeId);
         }
       }}
     >
-      <Table.Td>{probs.route.startAddress.name}</Table.Td>
-      <Table.Td>{probs.route.endAddress.name}</Table.Td>
-      <Table.Td>{probs.route.distance} KM</Table.Td>
+     
+        <HoverCard width={250} shadow="md">
+          <HoverCard.Target>
+            <Table.Td>{probs.route.startAddress.name}</Table.Td>
+          </HoverCard.Target>
+          <HoverCard.Dropdown>
+            <Grid>
+              <Grid.Col span={8}>{probs.route.startAddress.street}</Grid.Col>
+            </Grid>
+            <Grid>
+              <Grid.Col span={4}>{probs.route.startAddress.houseNumber}</Grid.Col>
+            </Grid>
+            <Grid>
+              <Grid.Col span={4}>{probs.route.startAddress.plz}</Grid.Col>
+            </Grid>
+            <Grid>
+              <Grid.Col span={8}>{probs.route.startAddress.place}</Grid.Col>
+            </Grid>
+          </HoverCard.Dropdown>
+        </HoverCard>
+  
+
+   
+        <HoverCard width={300} shadow="md">
+          <HoverCard.Target>
+            <Table.Td>{probs.route.endAddress.name}</Table.Td>
+          </HoverCard.Target>
+          <HoverCard.Dropdown>
+          <Grid>
+              <Grid.Col span={8}>{probs.route.endAddress.street}</Grid.Col>
+            </Grid>
+            <Grid>
+              <Grid.Col span={4}>{probs.route.endAddress.houseNumber}</Grid.Col>
+            </Grid>
+            <Grid>
+              <Grid.Col span={4}>{probs.route.endAddress.plz}</Grid.Col>
+            </Grid>
+            <Grid>
+              <Grid.Col span={8}>{probs.route.endAddress.place}</Grid.Col>
+            </Grid>
+          </HoverCard.Dropdown>
+        </HoverCard>
+      
+
+     
+            <Table.Td>{probs.route.distance} KM</Table.Td>
+       
     </Table.Tr>
   );
 }
