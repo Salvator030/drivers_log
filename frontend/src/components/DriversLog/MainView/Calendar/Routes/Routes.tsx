862e9042520334e70bc98e9809876110b6c5a075
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2025-04-15 03:40:55 +0200

    add Accordion

--- frontend/src/components/DriversLog/MainView/Calendar/Routes/Routes.tsx
+++ frontend/src/components/DriversLog/MainView/Calendar/Routes/Routes.tsx
@@ -1,9 +1,4 @@
-import {
-  Button,
-  Group,
-  Stack,
-  Title,
-} from "@mantine/core";
+import { Accordion, Button, Group, Stack, Title } from "@mantine/core";
 import { RoutesTable } from "../RotesTable/RoutesTable";
 import { useTranslation } from "react-i18next";
 import { useRoute } from "../../../../../hooks/useRoute";
@@ -16,22 +11,33 @@ interface RouteProbs {
 
 export function Routes(probs: RouteProbs) {
   const { t } = useTranslation();
-const { handleSaveRoutesBtn,handelNewRoteBtn, tableBody } = useRoute();
-
-
+  const { handleSaveRoutesBtn, handelNewRoteBtn, tableBody } = useRoute();
 
   return (
     <>
-      <Stack>
-        <Title order={3}>{t("navbarSimple.lable.routes")}</Title>
-        <RoutesTable tableBody={tableBody} />
-        <Group>
-          <Button onClick={() => handleSaveRoutesBtn(probs.dateRange)}>
-          {t("route.btn.addRote")}
-          </Button>
-          <Button onClick={handelNewRoteBtn}> {t("route.btn.newRoute")}</Button>
-        </Group>
-      </Stack>
+      <Accordion>
+        <Accordion.Item value="strecken">
+          <Accordion.Control>
+          <Title order={3}>  {t("navbarSimple.lable.routes")}</Title>
+          </Accordion.Control>
+          <Accordion.Panel>
+            {" "}
+            <Stack>
+             
+              <RoutesTable tableBody={tableBody} />
+              <Group>
+                <Button onClick={() => handleSaveRoutesBtn(probs.dateRange)}>
+                  {t("route.btn.addRote")}
+                </Button>
+                <Button onClick={handelNewRoteBtn}>
+                  {" "}
+                  {t("route.btn.newRoute")}
+                </Button>
+              </Group>
+            </Stack>
+          </Accordion.Panel>
+        </Accordion.Item>
+      </Accordion>
     </>
   );
 }
