a279d53d5a50b5a2011c74cc4b4067b50e788ffc
Salvator030 <108727933+Salvator030@users.noreply.github.com>
2025-04-15 03:03:33 +0200

    change btn

--- frontend/src/components/DriversLog/MainView/Calendar/DrivenRoutes/DrivenRoutes.tsx
+++ frontend/src/components/DriversLog/MainView/Calendar/DrivenRoutes/DrivenRoutes.tsx
@@ -1,7 +1,8 @@
-import { px, Stack, Title } from "@mantine/core";
+import { ActionIcon, px, Stack, Title } from "@mantine/core";
 import { useTranslation } from "react-i18next";
 import { RoutesTable } from "../RotesTable/RoutesTable";
 import { useDrivenRoute } from "../../../../../hooks/useDrivenRoute";
+import { IconTrash } from "@tabler/icons-react";
 
 
 interface DrivenRoutesComponentProps {
@@ -9,12 +10,15 @@ interface DrivenRoutesComponentProps {
 }
 export function DrivenRoutes(probs: DrivenRoutesComponentProps){
        const { t, i18n } = useTranslation();
-       const {tableBody} = useDrivenRoute(probs.datesRangeValue);
+       const {tableBody, handleOnClickDeleteDrivenRoutes} = useDrivenRoute(probs.datesRangeValue);
       
 
 
        return ( <Stack ml={px(16)} mt={px(8)}><Title order={3}>{t('drivenRoutes.title')}</Title>
-                 <RoutesTable tableBody={tableBody}/>
+                 <RoutesTable tableBody={tableBody} />
+                 <ActionIcon onClick={handleOnClickDeleteDrivenRoutes}>
+                     <IconTrash/>
+                 </ActionIcon>
 
        </Stack>)
 }
